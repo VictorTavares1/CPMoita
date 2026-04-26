213a9bde7d6514a7f2aa28c9dacc6fb136fa0dc1
victor tavares <victortavares649@gmail.com>
2026-04-26 13:24:05 +0100

    fix: erpi page text identical to original, footer fixed

--- src/app/pages/servicos/erpi/erpi.ts
+++ src/app/pages/servicos/erpi/erpi.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
-import { ServicesListService, Service } from '../../../services/services-list';
-import { environment } from '../../../../environments/environment';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-erpi',
@@ -9,19 +7,6 @@ import { environment } from '../../../../environments/environment';
   styleUrl: './erpi.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Erpi implements OnInit {
-  readonly downloadBase = `${environment.uploadsUrl}/download.php?file=`;
-  service: Service | null = null;
-
-  constructor(private servicesListService: ServicesListService, private cdr: ChangeDetectorRef) {}
-
-  ngOnInit(): void {
-    this.servicesListService.getServices().subscribe({
-      next: (list) => {
-        this.service = list.find(s => s.titulo.toLowerCase().includes('erpi')) ?? null;
-        this.cdr.markForCheck();
-      },
-      error: (err) => console.error('Erro ao carregar serviço:', err),
-    });
-  }
+export class Erpi {
+  readonly downloadBase = 'http://localhost/centro-paroquial-moita/db/download.php?file=';
 }
